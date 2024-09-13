fdc3fe3f1b71aaf2e723987a1f1eb6eb5419657f
Florian <fr@ruffner-beratung.de>
2024-09-13 19:18:34 +0200

    set up

---
+++ Fallstudie_Backend/src/main/java/com/example/Fallstudie/FallstudieApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fallstudie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallstudieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallstudieApplication.class, args);
+	}
+
+}
---
+++ Fallstudie_Backend/src/test/java/com/example/Fallstudie/FallstudieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fallstudie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallstudieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
