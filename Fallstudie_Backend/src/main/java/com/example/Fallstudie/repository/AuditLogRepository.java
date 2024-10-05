b145d2826408a70a34930ba872308784b0331e3d
Florian <fr@ruffner-beratung.de>
2024-10-05 13:34:40 +0200

    security config und anpasssung von Login

--- Fallstudie_Backend/src/main/java/com/example/Fallstudie/repository/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Fallstudie.repository;
-
-import com.example.Fallstudie.model.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
