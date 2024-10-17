5166399d049e1eb23649e376196f4761cc71860c
Florian <fr@ruffner-beratung.de>
2024-10-17 13:20:14 +0200

    forecast ins DTO hinzugefügt.

--- Fallstudie_Frontend/src/DTO/UserDTO.jsx
+++ Fallstudie_Frontend/src/DTO/UserDTO.jsx
@@ -1,14 +1,14 @@
  class UserDTO {
 
   
-     constructor( id,  email,  firstName,  lastName,  password,  roleID) {
+     constructor( id,  email,  firstName,  lastName,  password,  roleID, forecast) {
         this.id = id;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
         this.roleID = roleID;
-    
+        this.forecast = forecast;
 }
  }
  export default UserDTO;
