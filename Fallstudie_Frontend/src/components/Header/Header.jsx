8621deda2bd074e1cc88f172c5147c911ec3e6ac
Florian <fr@ruffner-beratung.de>
2024-10-22 19:56:43 +0200

    header angepasst

--- Fallstudie_Frontend/src/components/Header/Header.jsx
+++ Fallstudie_Frontend/src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css';
 import Navbar from '../Navbar/Navbar';
+import companyLogo from "../../assets/company-logo.png";
 
 const Header = () => {
     return (
         <header className="header">
-            <div className="logo">APS</div>
-            <Navbar />
+            <img src={companyLogo} alt="Firmenlogo" className="company-logo"/>
+            <Navbar/>
         </header>
     );
 };
